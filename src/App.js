642173c5baa1cf2ab7d5e4a5ec1981ba0f31f055
Joshua Proctor <joshuaeup@gmail.com>
2021-03-21 17:26:54 -0400

    Added front end structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div id="main-container">
+            <h1 id="main-container__title">C.L.E.O</h1>
+            <div className="loop-structure" id="loop-1"></div>
+            <div className="loop-structure" id="loop-2"></div>
+            <div className="loop-structure" id="loop-3"></div>
+        </div>
+    );
+}
+
+export default App;
