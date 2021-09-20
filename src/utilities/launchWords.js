5c2de1a66bcde3bbbea036badb3844081c95ddae
Joshua Proctor <81370795+joshuaeup-rv@users.noreply.github.com>
2021-09-20 10:09:06 -0400

    feat: Implement additional input words (#4)

--- src/utilities/launchWords.js
+++ src/utilities/launchWords.js
@@ -1,17 +1,61 @@
-// Variables
 const recipes = ["pbj", "chicken and broccoli stir-fry", "meatball sub"];
 
-// Command Words
 const greetings = [
     "hello cleo",
     "hello clio",
     "hi cleo",
     "hi clio",
-    "he cleo",
-    "he clio",
     "hey cleo",
     "hey clio",
 ];
+
+const time = [
+    "what time is it",
+    "what's the time",
+    "can you tell me what time it is",
+];
+
+const date = [
+    "what date is it",
+    "what's the date",
+    "what's today's date",
+    "what is today's date",
+];
+
+const gratitude = [
+    "thank you cleo",
+    "thanks cleo",
+    "i appreciate you cleo",
+    "i appreciate it cleo",
+    "thank you",
+    "thank you very much",
+];
+
+const cookingRequest = [
+    "i need help cooking",
+    "i need some help cooking",
+    "i would like some help cooking",
+    "i could use some help cooking",
+];
+
+const recipeRequest = [
+    "how many recipes do you know",
+    "which recipes can i choose from",
+    "which recipes do you know",
+    "what are my recipe choices",
+    "what are my recipe options",
+    "what recipes do you know",
+];
+
 const aborts = ["abort cleo", "abort clio"];
 
-export { recipes, greetings, aborts };
+export {
+    recipes,
+    recipeRequest,
+    greetings,
+    aborts,
+    time,
+    date,
+    gratitude,
+    cookingRequest,
+};
